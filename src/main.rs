6b41cc91bb2650bad006769681c793105033b51e
AlexShem <alex.shemendyuk@gmail.com>
2025-08-22 22:42:15 +0200

    Stage 2 #RG1
    - Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -8,10 +10,17 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connection(_stream).expect("Failed to handle the connection");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
+    let response = bytes::Bytes::from("+PONG\r\n");
+    stream.write_all(&response)?;
+    Ok(())
 }
